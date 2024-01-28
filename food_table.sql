f914709856c0c5b64fc0b2bc67046bdba12e21eb
FlorinKola <150366089+FlorinKola@users.noreply.github.com>
2024-01-28 09:22:11 -0800

    Update food_table.sql

--- food_table.sql
+++ food_table.sql
@@ -1,21 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 17, 2021 at 12:59 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `food_table`
